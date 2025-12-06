03f4d596e23c229e22527130289d988251d9a4bb
Souleymane Doucoure <doucoures569@gmail.com>
2025-12-06 20:27:48 +0300

    Added Javadoc to Customer class

--- src/com/rental/model/Customer.java
+++ src/com/rental/model/Customer.java
@@ -1,9 +1,18 @@
 package com.rental.model;
 
+/**
+ * Represents a customer in the rental system.
+ * Stores basic personal information.
+ */
 public class Customer {
     private String customerId;
     private String name;
 
+    /**
+     * Constructor to create a new Customer.
+     * @param customerId Unique ID of the customer
+     * @param name Full name of the customer
+     */
     public Customer(String customerId, String name) {
         this.customerId = customerId;
         this.name = name;
