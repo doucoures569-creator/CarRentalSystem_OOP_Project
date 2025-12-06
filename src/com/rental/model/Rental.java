c00658c663059d7690cfbc2ac2211dd1ee6b6a42
Souleymane Doucoure <doucoures569@gmail.com>
2025-12-06 20:34:59 +0300

    Added Javadoc to Rental transaction class

--- src/com/rental/model/Rental.java
+++ src/com/rental/model/Rental.java
@@ -1,31 +1,31 @@
 package com.rental.model;
 
+/**
+ * Represents a rental transaction linking a car to a customer.
+ * Automatically calculates and stores the final fee upon creation.
+ */
 public class Rental {
     private Car car;
     private Customer customer;
     private int days;
     private double rentalFee;
 
+    /**
+     * Creates a new Rental contract.
+     * @param car The car being rented
+     * @param customer The customer renting the car
+     * @param days Duration of the rental in days
+     */
     public Rental(Car car, Customer customer, int days) {
         this.car = car;
         this.customer = customer;
         this.days = days;
-        this.rentalFee = car.calculateRentalFee(days); // Le calcul se fait automatiquement ici
+        // Logic: Calculate fee immediately using the car's specific formula
+        this.rentalFee = car.calculateRentalFee(days); 
     }
 
-    public Car getCar() {
-        return car;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public int getDays() {
-        return days;
-    }
-
-    public double getRentalFee() {
-        return rentalFee;
-    }
+    public Car getCar() { return car; }
+    public Customer getCustomer() { return customer; }
+    public int getDays() { return days; }
+    public double getRentalFee() { return rentalFee; }
 }
