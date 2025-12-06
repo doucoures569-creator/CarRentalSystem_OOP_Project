09128ff5eccd8a41248b53ea73ba7afbc78a171c
Souleymane Doucoure <doucoures569@gmail.com>
2025-12-06 18:57:33 +0300

    Added Javadoc documentation to Rentable interface

--- src/com/rental/interfaces/Rentable.java
+++ src/com/rental/interfaces/Rentable.java
@@ -1,5 +1,15 @@
 package com.rental.interfaces;
 
+/**
+ * Interface representing any item that can be rented in the system.
+ * Enforces the calculation of rental fees.
+ */
 public interface Rentable {
+    
+    /**
+     * Calculates the cost of renting the item.
+     * @param days Number of rental days
+     * @return The total cost
+     */
     double calculateRentalFee(int days);
 }
